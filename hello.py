fa02573ca9960e5d40542452060ecdba1dbe2a33
Mateus <mateusdias52@gmail.com>
2024-01-08 10:42:34 -0300

    instaurando o multi linguas

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
-print('Hello World')
-print(1+1)
-print(2+2)
+import os
+
+current_language = os.getenv("LANG")
+
+msg = "Hello, World"
+
+if current_language == "pt_BR":
+    msg = "Ola, mundo"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo"    
+elif current_language == "es_SP":
+    msg = "Hola, Mundo"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg)
